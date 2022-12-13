1882352f720f2c798f37209f868c9f0ef670e65b
Daniel <69319302+DanielHenderson-17@users.noreply.github.com>
2022-12-13 14:13:48 -0600

    first

---
+++ file.sql
@@ -0,0 +1,10 @@
+-- The DISTINCT Keyword
+
+
+-- At the conclusion of a bank transaction, 6 checks are printed -- 3 originals for the bank, and 3 copies for the customer.
+-- Here is the list of all the checks named OperationcontentImageWrite a query to show only the unique checks that are given to the customer.
+-- Use the DISTINCT statement to return the unique values of mentioned column.
+
+-- 12/13/2022
+
+SELECT DISTINCT Checks FROM Operation
